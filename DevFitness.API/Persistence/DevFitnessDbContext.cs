bf93c5e07cb20ac1b46b3df55cc21918ae12e33c
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2021-06-06 17:52:15 -0300

    adicionando orm

---
+++ DevFitness.API/Persistence/DevFitnessDbContext.cs
@@ -0,0 +1,25 @@
+﻿using DevFitness.API.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFitness.API.Persistence
+{
+    public class DevFitnessDbContext : DbContext
+    {
+        public DevFitnessDbContext(DbContextOptions<DevFitnessDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Meal> Meals { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
