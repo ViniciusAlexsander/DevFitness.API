381d954abc98b1a4fb11b69c09c6d56622870fe8
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2021-06-06 13:04:21 -0300

    Add project files.

---
+++ DevFitness.API/Models/ViewModels/MealViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFitness.API.Models.ViewModels
+{
+    public class MealViewModel
+    {
+        public MealViewModel(string description, int calories, DateTime date, int id)
+        {
+            Description = description;
+            Calories = calories;
+            Date = date;
+            Id = id;
+        }
+
+        public string Description { get; private set; }
+        public int Calories { get; private set; }
+        public DateTime Date { get; private set; }
+        public int Id { get; private set; }
+    }
+}
